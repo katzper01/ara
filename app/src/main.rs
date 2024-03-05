a2b0358ea7e6bc0858c434364565ca72110a0e77
katzper01 <katzper.michno@gmail.com>
2024-03-05 13:54:49 +0100

    Changed gtk to dioxus

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,26 +1,30 @@
-extern crate cairo;
+#![allow(non_snake_case)]
 
-mod draw;
-mod io;
-mod plane_graph;
-mod service;
-mod window;
+use dioxus::prelude::*;
 
-use gtk::prelude::*;
-use gtk::{gio, glib, Application};
-use window::Window;
+pub mod io;
+pub mod plane_graph;
+pub mod service;
 
-const APP_ID: &str = "org.gtk_rs.ara";
+fn main() {
+    launch(App);
+}
 
-fn main() -> glib::ExitCode {
-    gio::resources_register_include!("ara.gresource").expect("Failed to register resources.");
+fn App() -> Element {
+    let mut text = use_signal(|| String::from("Hello world!"));
 
-    let app = Application::builder().application_id(APP_ID).build();
+    rsx! {
+        SimpleDiv {
+            text
+        }
+        button {
+            onclick: move |_| *text.write() = String::from("Other text"),
+            "Change text"
+        }
+    }
+}
 
-    app.connect_activate(|app: &Application| {
-        let window = Window::new(app);
-        window.present();
-    });
-
-    app.run()
+#[component]
+fn SimpleDiv(text: Signal<String>) -> Element {
+    rsx! { div { "{text}" } }
 }
